4f9e44ad386da3ee6608912007fa440729bfdf02
Girish S <girish@gravityilabs.com>
2018-09-19 18:39:05 +0530

    asas

--- demo/assets/js/scripts.js
+++ demo/assets/js/scripts.js
@@ -486,7 +486,7 @@ function projectBtnClick() {
     case 1:
       projectsHead = 'Projects';
       stageHead = 'Stage';
-      $('#planHead span').text('Delivery Program');
+      $('#planHead span').text('Plan Delivery Program');
       $('#seachProjectDataTable').attr('placeholder', 'Search by Name, Infrastructure Class, Region etc');
       for (let i = 1; i < 11; i++) {
         projectDataTable.column(i).visible(true);
